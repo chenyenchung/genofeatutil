64a9293af514cf05c3123b67a5afb09be115b7f5
chenyenchung <chenyenjung@gmail.com>
2019-03-09 18:53:16 -0500

    1. Re-write get_10Xgenename() as an S3 generic function get_gene_names() taking a matrix, data frame or a Seuratv3 object. 2. Allow users to use either row or col in calculate_gene_cor() and to pass method to cor() 3. Write test and documentation for utilities

--- R/Utilities.R
+++ R/Utilities.R
@@ -1,4 +1,4 @@
-#' Generate a tuning series of \code{mtry} for \code{randomForest()}
+#' Generate a Tuning Series of \code{mtry} for \code{randomForest()}
 #'
 #' \code{\link{randomForest}()} implements a default value for mtry determined
 #' by the number of predictors used. To examine the influence of mtry on
@@ -7,17 +7,19 @@
 #' Let default value be N, \code{make_mtry_series()} makes a numeric vector
 #' consisting 10, N/2, N, 2N, and the number of predictors provided.
 #' the number of predictors.
-#' @param x Predictor matrix
-#' @param y Response matrix
+#' @param x a predictor matrix
+#' @param y a response matrix
 #'
-#' @return A numeric factor
+#' @return a numeric factor
 #' @export
 #'
 #' @examples
 #' make_mtry_series(x = iris[ , 1:4], y = iris[ , 5])
 make_mtry_series <- function(x, y) {
   # default is the default value of mtry of randomForest::randomForest()
-  default <- if (!is.null(y) && !is.factor(y)) {max(floor(ncol(x)/3), 1)} else {floor(sqrt(ncol(x)))}
+  default <- if (!is.null(y) && !is.factor(y)) {
+    max(floor(ncol(x)/3), 1)
+    } else {floor(sqrt(ncol(x)))}
 
   # Make sure 2N does not exceed the total number of predictors
   if (ncol(x) > 2 * default) {
@@ -36,20 +38,122 @@ make_mtry_series <- function(x, y) {
   return(mtry_series)
 }
 
-gene_cor <- function(data, gene.1, gene.2) {
+#' Calculate Correlation Coefficient from an Expression Matrix
+#'
+#' \code{calculate_gene_cor()} takes an expression matrix. Two genes will be
+#' selected by column/row names and Pearson's correlation will be calculated
+#' by \code{\link{cor}()}.
+#'
+#' @param data a matrix or data.frame
+#' @param gene1,gene2 a character string gene name that's used as a column
+#' name in the expression matrix
+#' @param use.row by default FALSE, if TRUE, calculate_gene_cor will take
+#' row names as gene names; if FALSE, column names will be seens as gene names
+#' @param method a character string indicating which correlation coefficient to
+#' compute (Options: "\code{pearson}" (default), "\code{kendall}",
+#' or "\code{spearman}")
+#'
+#' @return a number
+#' @export
+#'
+#' @examples
+#' genedf <- data.frame("a" = c(1:10),
+#'                      "b" = c(6:14))
+#' calculate_gene_cor(genedf, "a", "b")
+calculate_gene_cor <- function(data, gene1, gene2,
+                               use.row = FALSE, method = "pearson") {
   # Calculate correlation of genes from an expression matrix
   # The genes are selected based on column names
-  gene1exp <- as.numeric(data[ , gene.1])
-  gene2exp <- as.numeric(data[ , gene.2])
-  rcoef <- stats::cor(gene1exp, gene2exp)
+  if (!is.matrix(data) & !is.data.frame(data)) {
+    stop(strwrap("calculate_gene_core only takes a matrix or
+                 a data frame as input data."))
+  }
+  if (use.row) {
+    gene_names <- row.names(data)
+  } else {
+    gene_names <- colnames(data)
+  }
+
+  # Make sure the gene names are in the expression matrices
+  if (!(gene1 %in% gene_names)) {
+    stop(gene1, " is not found in the expression matrix.")
+  }
+  if (!(gene2 %in% gene_names)) {
+    stop(gene2, " is not found in the expression matrix.")
+  }
+
+  if (use.row) {
+    gene1exp <- as.numeric(data[gene1, ])
+    gene2exp <- as.numeric(data[gene2, ])
+  } else {
+    gene1exp <- as.numeric(data[ , gene1])
+    gene2exp <- as.numeric(data[ , gene2])
+  }
+  rcoef <- stats::cor(gene1exp, gene2exp, method = method)
   return(rcoef)
 }
 
-get_10Xgenenames <- function(object, assay = "RNA") {
+#' Get Gene Names from an Expression Matrix or a Seurat object
+#'
+#' \code{get_gene_names()} extracts gene names from a matrix, data frame, or
+#' Seurat object. For a martix or data frame, you need to tell it whether the
+#' gene names are in row names or column names. If it's a Seurat object,
+#' you'll need to specify the assay you want.
+#' @param x an object to extract gene names from
+#'
+#' @return a character vector
+#' @export
+#'
+#' @examples
+#' # Create a dummy expression matrix
+#' expression_df <- data.frame(
+#' "sample1" = c(1:10),
+#' "sample2" = c(1:10),
+#' "sample3" = c(1:10)
+#' )
+#'
+#' row.names(expression_df) <- c(paste("gene", c(1:10), sep = ""))
+#'
+#' # Create Seurat object from the dummy data
+#' seuratobj <- Seurat::CreateSeuratObject(counts = expression_df,
+#'                                         assay = "RNA")
+#' # Get gene names from a data frame
+#' get_gene_names(expression_df, use.row = TRUE)
+#'
+#' # Get gene names from a Seuratv3 object
+#' get_gene_names(seuratobj, assay = "RNA")
+get_gene_names <- function(x, ...) {
+  UseMethod("get_gene_names", x)
+}
+
+#' @param x an expression matrix
+#' @param use.row a logical constant indicating whether the gene names are in
+#' row names or not
+#'
+#' @rdname get_gene_names
+get_gene_names.default <- function(x, use.row = FALSE) {
+  # Type check
+  if (!(is.data.frame(x) | is.matrix(x))) {
+    stop("Sorry, the object is not supported by get_gene_names().")
+  }
+  if (use.row) {
+    return(row.names(x))
+  }
+  return(colnames(x))
+}
+
+#' @param object a Seuratv3 object
+#' @param assay a character string indicating an assay in the Seurat object
+#' @param slot a character string indicating a data slot in the Seurat object
+#' (Options: \code{counts}, \code{data}, or \code{scale.data})
+#'
+#' @rdname get_gene_names
+get_gene_names.Seurat <- function(x, assay = "RNA") {
   # Extract the names of all detected genes of an assay in a Seuratv3 object
-  multiassayobj <- methods::slot(object, name = "assays")
+  multiassayobj <- methods::slot(x, name = "assays")
   singleassayobj <- multiassayobj[[assay]]
-  assayobj <- GetAssayData(object = singleassayobj, assay.type = assay, slot = "data")
+  assayobj <- Seurat::GetAssayData(object = singleassayobj,
+                           assay.type = assay, slot = "data")
   genenames_10X <- row.names(assayobj)
   return(genenames_10X)
 }
--- tests/testthat/test-utilities.R
+++ tests/testthat/test-utilities.R
@@ -1,5 +1,6 @@
 context("test-utilities")
 
+# Test make_mtry_series() --------------
 test_that("mtry series", {
   df <- data.frame("x1" = c(1:10),
                    "x2" = c(1:10),
@@ -15,4 +16,72 @@ test_that("mtry series", {
                    )
   expect_equal(make_mtry_series(x = df[ , 1:9], y = df[ , 10]), c(1, 3, 6, 9))
   expect_equal(make_mtry_series(x = df[ , 1:4], y = df[ , 11]), c(1, 2, 4))
+})
+
+# Test calculate_gene_cor() ------------------
+test_that("calculate_gene_cor", {
+  genedf <- data.frame("a" = c(1:10),
+                       "b" = c(6:15))
+  genedf_r <- t(genedf)
+
+  # Check consistency with cor()
+  ## Pearson
+  expect_equal(calculate_gene_cor(genedf, "a", "b"),
+               cor(genedf$a, genedf$b))
+  ## Kendall
+  expect_equal(calculate_gene_cor(genedf, "a", "b", method = "kendall"),
+               cor(genedf$a, genedf$b, method = "kendall"))
+  ## Spearman
+  expect_equal(calculate_gene_cor(genedf, "a", "b", method = "spearman"),
+               cor(genedf$a, genedf$b, method = "spearman"))
+
+  ## Check gene names as row names
+  expect_equal(calculate_gene_cor(genedf_r, "a", "b", use.row = TRUE),
+               cor(genedf$a, genedf$b))
+  ## Kendall
+  expect_equal(calculate_gene_cor(genedf_r, "a", "b",
+                                  method = "kendall", use.row = TRUE),
+               cor(genedf$a, genedf$b, method = "kendall"))
+  ## Spearman
+  expect_equal(calculate_gene_cor(genedf_r, "a", "b",
+                                  method = "spearman", use.row = TRUE),
+               cor(genedf$a, genedf$b, method = "spearman"))
+
+  # Check non-existent gene name handling
+  expect_error(calculate_gene_cor(genedf, "a", "c"))
+
+  # Check data type detection
+  expect_error(calculate_gene_cor(genedf$a, "a", "b"))
+
+})
+
+# Test get_gene_names() (S3 generic) ------------------
+test_that("get_gene_names", {
+  # Create a dummy expression matrix
+  expression_df <- data.frame(
+    "sample1" = c(1:10),
+    "sample2" = c(1:10),
+    "sample3" = c(1:10)
+  )
+  row.names(expression_df) <- c(paste("gene", c(1:10), sep = ""))
+
+  # Create Seurat object from the dummy data
+  seuratobj <- Seurat::CreateSeuratObject(counts = expression_df,
+                                          assay = "RNA")
+  # Check type
+  expect_error(get_gene_names(list()))
+  # Check data.frame
+  expect_equal(get_gene_names(expression_df, use.row = T),
+               c(paste("gene", c(1:10), sep = ""))
+               )
+  # Check matrix
+  expect_equal(get_gene_names(t(expression_df)),
+               c(paste("gene", c(1:10), sep = ""))
+               )
+
+  # Check Seurat object
+  expect_equal(get_gene_names(seuratobj,
+                              assay = "RNA"),
+               c(paste("gene", c(1:10), sep = ""))
+               )
 })
